2fd207711c6808a7162241577de4e99c53f737cb
sea-yassan33 <a.yasuda_tr51@me.com>
2023-03-11 19:36:16 +0900

    get_data_api

--- js/main.js
+++ js/main.js
@@ -8,10 +8,32 @@ const app = Vue.createApp({
 
   },
   mounted: function(){
-
+    this.keyword = 'JavaScript'
+    this.getAnswer()
   },
   methods: {
+    getAnswer: function(){
+      if(this.keyword === ''){
+        console.log('Nul')
+        this.items = null
+        return
+      }
 
+      this.message = 'Lading...'
+      const vm = this
+      const params = { page: 1, per_page: 20, query: this.keyword }
+      axios.get('https://qiita.com/api/v2/items', { params })
+           .then(function(response){
+             //console.log(response)
+            vm.items = response.data
+           })
+           .catch(function(error){
+            vm.message = 'Error!' + error
+           })
+           .finally(function(){
+            vm.message = ''
+           })
+    }
   }
 })
 app.mount('#app')
