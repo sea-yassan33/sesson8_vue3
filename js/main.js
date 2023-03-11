ef5bf150c199a0f015e30287c3891c40c0574d00
sea-yassan33 <a.yasuda_tr51@me.com>
2023-03-11 18:56:22 +0900

    preparation_option

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 const app = Vue.createApp({
   data: () => ({
+    items: null,
+    keyword: '',
+    message: ''
+  }),
+  watch: {
 
-  })
+  },
+  mounted: function(){
+
+  },
+  methods: {
+
+  }
 })
 app.mount('#app')
